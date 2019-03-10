c5befb426b75c829c530a95676d20d4c642e9986
Andres Alonso <andrescalonso@gmail.com>
2019-03-10 16:34:45 -0400

    Changed to AreaChart and added tooltip

--- src/components/DailyChart.tsx
+++ src/components/DailyChart.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import {
-  LineChart,
-  Line,
+  AreaChart,
+  Area,
   CartesianGrid,
   XAxis,
   YAxis,
+  Tooltip,
   ResponsiveContainer,
 } from 'recharts'
 
@@ -14,12 +15,18 @@ export const DailyChart = ({ symbol, data }: any) => {
     <div>
       <div className="SP__chartLabel">{`${symbol}`}</div>
       <ResponsiveContainer width="100%" height={350}>
-        <LineChart className="SP__chart" data={data}>
-          <Line type="monotone" dataKey="close" stroke="#8884d8" />
+        <AreaChart className="SP__chart" data={data}>
+          <Area
+            type="monotone"
+            dataKey="close"
+            stroke="#4caf50"
+            fill="#4caf50"
+          />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="tradingDay" />
           <YAxis domain={['dataMin - 5', 'dataMax + 5']} />
-        </LineChart>
+          <Tooltip />
+        </AreaChart>
       </ResponsiveContainer>
     </div>
   )
